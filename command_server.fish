703f8b7949477493629cd2a3c3286f63a407682e
Subhav Ramachandran <subhav@users.noreply.github.com>
2021-05-03 16:39:42 -0400

    Add "Done" to command_server.fish responses

--- command_server.fish
+++ command_server.fish
@@ -2,11 +2,15 @@
 # in fish like command substitutions run synchronously and change state.
 
 set -e LD_PRELOAD
+set -e SETPGRP_FD
 
 function __cmd_trap_int --on-signal INT
     return
 end
 
+# `job-control interactive` (the default) is supposed to enable job control in interactive mode.
+# However, it doesn't seem to actually set the foreground process group with scripts and `-i`.
+# Not sure if it really works at all.
 status --job-control full
 
 set __cmd_stdin ""
@@ -25,11 +29,12 @@ end
 set __cmd_last_status $status
 # eval in fish used to be a function which piped the command into source.
 # We can do the same thing, simplifying its behavior.
+# Some fish commands (e.g. "time") and errors write to stderr no matter what, ignoring redirections
 function __cmd_run -a command --no-scope-shadowing
 	__cmd_restore_status $__cmd_last_status
 	echo "begin $command "\n" ;end <$__cmd_stdin" | source >$__cmd_stdout 2>$__cmd_stderr
 	set -g __cmd_last_status $status
-	echo "{\"Exit\": $__cmd_last_status}"
+	echo "{\"Done\": true, \"Exit\": $__cmd_last_status}"
 end
 
 while read --null method args
